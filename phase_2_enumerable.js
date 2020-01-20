d13536d93685ddec7e40283655e8bde8ef6e64cc
Cihangir <akincicihangir@gmail.com>
2020-01-20 09:54:20 -0500

    Phase2_Done

--- phase_2_enumerable.js
+++ phase_2_enumerable.js
@@ -6,11 +6,42 @@ Array.prototype.myEach = function(func){
     }
 };
 
-array.myEach((num) => {
-    console.log(`square of ${num} is ${num * num}`);
-});
+// array.myEach((num) => {
+//     console.log(`square of ${num} is ${num * num}`);
+// });
 
 
-// NUMS.forEach((num) => {
+// array.forEach((num) => {
 //   console.log(`square of ${num} is ${num * num}`);
 // });
+
+Array.prototype.myMap = function(func){
+    let newArray = [];
+    this.myEach(element => newArray.push(func(element)));
+    return newArray;
+};
+
+// array.myMap((num) => {
+//     console.log(`square of ${num} is ${num * num}`);
+// });
+
+
+
+Array.prototype.myReduce = function(func, initialValue){
+    let result = initialValue;
+    
+    this.myEach(element => result = func(result, element));
+
+    return result;
+};
+
+
+// // without initialValue
+// [1, 2, 3].myReduce(function(acc, el) {
+//     return acc + el;
+//   }); // => 6
+  
+  // with initialValue
+  console.log([1, 2, 3].myReduce(function(acc, el) {
+    return acc + el;
+  }, 25)); // => 31
